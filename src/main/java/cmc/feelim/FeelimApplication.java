34e2c6987002fccfc374c2e4bdecccd28bfd0e33
minaamim <minaaa5814@gmail.com>
2022-09-15 14:31:25 +0900

    initial commit

---
+++ src/main/java/cmc/feelim/FeelimApplication.java
@@ -0,0 +1,13 @@
+package cmc.feelim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeelimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeelimApplication.class, args);
+	}
+
+}
