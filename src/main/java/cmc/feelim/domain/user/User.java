b991e600e97a41dd4909118f844553fcc9b9ba5f
minaamim <minaaa5814@gmail.com>
2022-09-23 15:09:37 +0900

    [add] Add Role to User

--- src/main/java/cmc/feelim/domain/user/User.java
+++ src/main/java/cmc/feelim/domain/user/User.java
@@ -7,6 +7,7 @@ import cmc.feelim.domain.film.Film;
 import cmc.feelim.domain.follow.Follow;
 import cmc.feelim.domain.likes.Likes;
 import cmc.feelim.domain.post.Post;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -24,6 +25,9 @@ public class User extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     @NotNull
     @Column(length = 45)
@@ -62,4 +66,15 @@ public class User extends BaseEntity {
     //내가 팔로우하는 유저
     @OneToMany(mappedBy = "toUser", cascade = CascadeType.ALL)
     private List<Follow> follows = new ArrayList<>();
+
+    public void createSocialId(String toUpperCase) {
+    }
+
+    @Builder
+    public User (String email, String pwd, String nickname, String name) {
+        this.email = email;
+        this.pwd = pwd;
+        this.nickname = nickname;
+        this.name = name;
+    }
 }
