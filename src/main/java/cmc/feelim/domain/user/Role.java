e3cdd51c9382f7b278db00b5880cce3abd5b0ee6
minaamim <minaaa5814@gmail.com>
2022-09-23 15:09:37 +0900

    [add] Add Role to User

---
+++ src/main/java/cmc/feelim/domain/user/Role.java
@@ -0,0 +1,13 @@
+package cmc.feelim.domain.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum Role {
+    ADMIN("ROLE_ADMIN"),
+    USER("ROLE_USER");
+
+    private String value;
+}
