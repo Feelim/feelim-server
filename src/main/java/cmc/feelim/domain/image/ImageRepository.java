f134af834d1e813c618522caad4d065a8e2e154e
minaamim <minaaa5814@gmail.com>
2022-09-30 15:11:36 +0900

    [feat] Create Image Upload feature

---
+++ src/main/java/cmc/feelim/domain/image/ImageRepository.java
@@ -0,0 +1,8 @@
+package cmc.feelim.domain.image;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImageRepository extends JpaRepository<Image, Long> {
+}
