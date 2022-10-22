6fb228883bfef0ad7053889928cea92432e278e1
minaamim <minaaa5814@gmail.com>
2022-10-22 17:13:24 +0900

    [feat] Create a feature to search laboratory

--- src/main/java/cmc/feelim/domain/laboratory/LaboratoryRepository.java
+++ src/main/java/cmc/feelim/domain/laboratory/LaboratoryRepository.java
@@ -2,6 +2,7 @@ package cmc.feelim.domain.laboratory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.awt.*;
@@ -10,4 +11,5 @@ import java.util.List;
 @Repository
 public interface LaboratoryRepository extends JpaRepository<ProcessingLaboratory, Long> {
 
+    List<ProcessingLaboratory> findByNameContaining(@Param("keyword") String keyword);
 }
