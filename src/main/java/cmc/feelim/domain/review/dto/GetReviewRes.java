4d901b83bad97ba8a41772a38ddb9d1b2d75e5f7
minaamim <minaaa5814@gmail.com>
2022-11-09 17:38:40 +0900

    [add] Add Image to Review

--- src/main/java/cmc/feelim/domain/review/dto/GetReviewRes.java
+++ src/main/java/cmc/feelim/domain/review/dto/GetReviewRes.java
@@ -1,8 +1,13 @@
 package cmc.feelim.domain.review.dto;
 
+import cmc.feelim.domain.image.Image;
+import cmc.feelim.domain.image.ImageDto;
 import cmc.feelim.domain.review.Review;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -12,6 +17,7 @@ public class GetReviewRes {
     private String nickname;
     private String content;
     private double star;
+    private List<ImageDto> images = new ArrayList<>();
 
     public GetReviewRes(Review review) {
         this.reviewId = review.getId();
@@ -19,5 +25,7 @@ public class GetReviewRes {
         this.nickname = review.getUser().getNickname();
         this.content = review.getContent();
         this.star = review.getStar();
+        review.getImages().stream()
+                .forEach(image -> {images.add(image.toDto());});
     }
 }
--- src/main/java/cmc/feelim/domain/review/dto/PostReviewReq.java
+++ src/main/java/cmc/feelim/domain/review/dto/PostReviewReq.java
@@ -2,10 +2,18 @@ package cmc.feelim.domain.review.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
 public class PostReviewReq {
+    @NotNull
     private double star;
+    @NotNull
     private String content;
+    private List<MultipartFile> images = new ArrayList<>();
 }
