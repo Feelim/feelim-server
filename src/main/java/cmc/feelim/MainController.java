9b7b3d1d15350f7e0fdc04112b248c71338bd241
minaamim <minaaa5814@gmail.com>
2023-06-22 18:44:27 +0900

    [feat] health check

--- src/main/java/cmc/feelim/MainController.java
+++ src/main/java/cmc/feelim/MainController.java
@@ -1,18 +1,20 @@
 package cmc.feelim;
 
+import cmc.feelim.config.exception.BaseException;
+import cmc.feelim.config.exception.BaseResponse;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@Slf4j
 @RestController
 public class MainController {
+    @ApiOperation("health check")
     @GetMapping("/")
-    public String main1() throws Exception{
-        return "hi";
-    }
-
-    @GetMapping("/hi2")
-    public String main2() throws Exception{
-        return "main";
+    public BaseResponse<String> checkHealth() throws BaseException {
+        return new BaseResponse<>("health check OK!");
     }
 
 }
